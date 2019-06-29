83a46e684c7c93490ddfb77fa36da8a81864d9a4
Rahul <alokrockrahul@gmail.com>
2019-06-29 21:10:31 +0530

    k

--- cart/shop/views.py
+++ cart/shop/views.py
@@ -1,7 +1,7 @@
 # Create your views here.
 from django.http import HttpResponse
 from django.shortcuts import render
-from .models import product,Contact
+from .models import product,Contact,Orders
 from math import ceil
 import logging
 
@@ -41,10 +41,22 @@ def search(request):
 def productView(request, myid):
     # Fetch the product using the id
     product1 = product.objects.filter(product_id=myid)
-
-
-    return render(request, 'shop/prodView.html', {'product':product1[0]})
+    return render(request, 'shop/prodview.html', {'product':product1[0]})
 def checkout(request):
+    if request.method=="POST":
+        items_json = request.POST.get('itemsJson', '')
+        name = request.POST.get('name', '')
+        email = request.POST.get('email', '')
+        address = request.POST.get('address1', '') + " " + request.POST.get('address2', '')
+        city = request.POST.get('city', '')
+        state = request.POST.get('state', '')
+        zip_code = request.POST.get('zip_code', '')
+        phone = request.POST.get('phone', '')
+        order = Orders(items_json=items_json, name=name, email=email, address=address, city=city,
+                       state=state, zip_code=zip_code, phone=phone)
+        order.save()
+        thank = True
+        id = order.order_id
+        return render(request, 'shop/checkout.html', {'thank':thank, 'id': id})
     return render(request, 'shop/checkout.html')
-
 # Create your views here.
