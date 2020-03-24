07fd8d5d43cbc2ad933c761ce01415b990675c5e
Rahul <alokrockrahul@gmail.com>
2020-03-24 22:42:01 +0530

    q

--- cart/cart/settings.py
+++ cart/cart/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -126,3 +127,5 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 # Managing media
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
